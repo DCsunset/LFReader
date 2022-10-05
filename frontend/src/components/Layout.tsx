b48ef22aa81cf44327355b3bb746ad69ea599d55
DCsunset <DCsunset@users.noreply.github.com>
2022-10-05 13:25:58 -0400

    feat(frontend): add responsive drawer

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,20 +1,25 @@
 import { useRecoilState } from "recoil";
 import { notificationState } from "../states/app";
 import { Notification } from "../types/states";
-import { Alert, AppBar, Box, Snackbar, Toolbar, Typography } from "@mui/material";
+import { Alert, AppBar, Box, Divider, Drawer, IconButton, Snackbar, Toolbar, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { useEffect, useState } from "react";
 import Icon from "@mdi/react";
-import { mdiRss } from "@mdi/js";
+import { mdiMenu, mdiRss } from "@mdi/js";
 
 interface Props {
 	children?: any
 }
+
+const drawerWidth = 240;
 
 function Layout(props: Props) {
 	const [notification, setNotification] = useRecoilState(notificationState);
 	// Local cache of notification (delayed destruction and update)
 	const [currentNotification, setCurrentNotification] = useState<Notification | null>(null);
 	const [snackbarOpen, setSnackbarOpen] = useState(false);
+	const [drawerOpen, setDrawerOpen] = useState(false);
+	const theme = useTheme();
+	const smallDevice = useMediaQuery(theme.breakpoints.down("sm"));
 
 	// Update notification on change
 	useEffect(() => {
@@ -29,17 +34,38 @@ function Layout(props: Props) {
 		}
 	}, [notification, currentNotification, snackbarOpen])
 
-	
+
 	const handleSnackbarClose = (_event: React.SyntheticEvent | Event, reason?: string) => {
 		if (reason === 'clickaway') {
 			return;
 		}
 		setSnackbarOpen(false);
-  };
-	
+	};
+
 	// Invalidate local cache only after the snackbar closes completely
 	const handleSnackbarExited = () => {
 		setCurrentNotification(null);
+	};
+
+	const responsiveStyles = {
+		// Keep the animation same as drawer
+		transition: theme.transitions.create(["margin", "width"], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		}),
+		...(drawerOpen && {
+			transition: theme.transitions.create(["margin", "width"], {
+				easing: theme.transitions.easing.easeOut,
+				duration: theme.transitions.duration.leavingScreen
+			}),
+			// Leave space for responsive drawer
+			width: {
+				sm: `calc(100% - ${drawerWidth}px)`
+			},
+			ml: {
+				sm: `${drawerWidth}px`
+			}
+		})
 	};
 
 	return (
@@ -48,17 +74,60 @@ function Layout(props: Props) {
 			display: "flex",
 			flexDirection: "column"
 		}}>
-			<AppBar position="sticky">
-				<Toolbar style={{ minHeight: "48px" }}>
-					<Icon
-						path={mdiRss}
-						size={1}
-					/>
+			<AppBar
+				position="sticky"
+				sx={responsiveStyles}
+			>
+				<Toolbar variant="dense" sx={{
+					minHeight: "48px",
+					// Keep padding consistent
+					px: { sm: 2 }
+				}}>
+					<IconButton onClick={() => setDrawerOpen(val => !val)}>
+						<Icon
+							path={mdiMenu}
+							size={1}
+						/>
+					</IconButton>
 					<Typography variant="h6" sx={{ ml: 1 }}>
-						yafr
+						Title
 					</Typography>
 				</Toolbar>
 			</AppBar>
+
+			<Box
+				component="nav"
+				sx={{
+					width: { sm: drawerWidth },
+					flexShrink: { sm: 0 }
+				}}
+			>
+				<Drawer
+					variant={smallDevice ? "temporary" : "persistent"}
+					anchor="left"
+					// Change width of paper component inside drawer
+					PaperProps={{
+						sx: {
+							width: drawerWidth
+						}
+					}}
+					// For better performance
+					keepMounted={true}
+					open={drawerOpen}
+					onClose={() => setDrawerOpen(false)}
+				>
+					<Toolbar>
+						<Icon
+							path={mdiRss}
+							size={1}
+						/>
+						<Typography variant="h6" sx={{ ml: 1 }}>
+							yafr
+						</Typography>
+					</Toolbar>
+					<Divider />
+				</Drawer>
+			</Box>
 
 			<Snackbar
 				anchorOrigin={{
@@ -81,9 +150,13 @@ function Layout(props: Props) {
 				</Alert>
 			</Snackbar>
 
-			<div style={{ height: "100%" }}>
+			<Box component="main" sx={{
+				flexGrow: 1,
+				height: "100%",
+				...responsiveStyles
+			}}>
 				{props.children}
-			</div>
+			</Box>
 		</Box>
 	);
 }
