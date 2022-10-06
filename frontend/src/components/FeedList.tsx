3f1c5478c538a698c4e1a659af46229b9acdb5ae
DCsunset <DCsunset@users.noreply.github.com>
2022-10-06 16:32:41 -0400

    feat(frontend): show list of feeds with icons

--- frontend/src/components/FeedList.tsx
+++ frontend/src/components/FeedList.tsx
@@ -1,9 +1,52 @@
-import { List, ListItem } from "@mui/material";
+import { Box, List, ListItem } from "@mui/material";
+import { useEffect, useState } from "react";
+import { getFeeds, Feed } from "../states/actions";
+import fetchIcon from "../utils/fetchIcon";
+
+export interface FeedWithIcon extends Feed {
+	icon?: string
+};
 
 function FeedList() {
+	const [feeds, setFeeds] = useState<FeedWithIcon[]>([]);
+
+	useEffect(() => {
+		const fetchFeeds = async () => {
+			const origFeeds= await getFeeds();
+			const newFeeds = await Promise.all(
+				origFeeds.map(async feed => ({
+					...feed,
+					icon: feed.link && await fetchIcon(feed.link)
+				} as FeedWithIcon))
+			);
+			setFeeds(newFeeds);
+		}
+
+		fetchFeeds();
+	}, []);
+
+	// useEffect(() => {
+	// 	console.log(feeds)
+	// }, [feeds]);
+
 	return (
 		<List>
-			<ListItem>Test</ListItem>
+			{feeds.map(feed => (
+				<ListItem key={feed.url}>
+					{feed.icon &&
+						<Box
+							component="img"
+							src={feed.icon}
+							sx={{
+								width: 20,
+								height: 20,
+								mr: 1
+							}}
+						/>
+					}
+					{feed.title ?? feed.link ?? feed.url}
+				</ListItem>
+			))}
 		</List>
 	);
 }
