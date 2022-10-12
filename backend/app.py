55fcef3a7984bdeefb1d163954407bf4378e7eef
DCsunset <DCsunset@users.noreply.github.com>
2022-10-12 12:03:48 -0400

    feat(backend): support tags for feed

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from reader import make_reader, Feed, Entry, FeedNotFoundError, StorageError, FeedExistsError, InvalidFeedURLError, ParseError
+from reader import make_reader, Reader, Feed, Entry, FeedNotFoundError, StorageError, FeedExistsError, InvalidFeedURLError, ParseError
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import PlainTextResponse
 from fastapi.exceptions import RequestValidationError
@@ -7,6 +7,7 @@ import logging
 import threading
 import os 
 from encoder import encode_data, decode_id, DecodeError
+from pydantic import BaseModel
 
 logging.basicConfig(level=logging.INFO)
 
@@ -25,51 +26,86 @@ app = FastAPI(**fastapi_args)
 # each thread must have a different reader because of SQLite3
 readers = {}
 
-def get_reader():
+def get_reader() -> Reader:
 	thread_id = threading.local()
 	if not (thread_id in readers):
 		readers[thread_id] = make_reader(YAFR_DB)
 	return readers[thread_id]
 
 ## Feed API
+
+class FeedArgs(BaseModel):
+	tags: list[str] | None = None
+
+
+"""
+Get feeds
+"""
 @app.get("/feeds")
-async def get_feeds():
+async def get_feeds_api():
 	reader = get_reader()
 	feeds = reader.get_feeds()
 	return encode_data(feeds, exclude_none=True)
 
+"""
+Get a feed
+"""
 @app.get("/feeds/{encoded_id}")
-async def get_feed(encoded_id):
+async def get_feed_api(encoded_id: str):
 	reader = get_reader()
-	feed_id = decode_id(encoded_id)
-	feed = reader.get_feed(feed_id)
+	feed_url = decode_id(encoded_id)
+	feed = reader.get_feed(feed_url)
 	return encode_data(feed, exclude_none=True)
 
+
+def update_feed(reader: Reader, feed_url: str, args: FeedArgs):
+	# set tag
+	if args.tags is not None:
+		orig_tags = reader.get_tag_keys(feed_url)
+		# delete old tags first
+		for tag in orig_tags:
+			reader.delete_tag(feed_url, tag)
+		# add new tags
+		for tag in args.tags:
+			reader.set_tag(feed_url, tag)
+
+	reader.update_feed(feed_url)
+
+
+"""
+Update a feed
+"""
 @app.put("/feeds/{encoded_id}")
-async def update_feed(encoded_id):
+async def update_feed_api(encoded_id: str, args: FeedArgs):
+	feed_url = decode_id(encoded_id)
 	reader = get_reader()
-	feed_id = decode_id(encoded_id)
-	reader.update_feed(feed_id)
+	update_feed(reader, feed_url, args)
 	return {}
 
+"""
+Add a new feed
+"""
 @app.post("/feeds/{encoded_id}")
-async def add_feed(encoded_id):
+async def add_feed_api(encoded_id: str, args: FeedArgs):
 	reader = get_reader()
-	feed_id = decode_id(encoded_id)
-	reader.add_feed(feed_id)
-	reader.update_feed(feed_id)
+	feed_url = decode_id(encoded_id)
+	reader.add_feed(feed_url)
+	update_feed(reader, feed_url, args)
 	return {}
 
+"""
+Delete a feed
+"""
 @app.delete("/feeds/{encoded_id}")
-async def delete_feed(encoded_id):
+async def delete_feed_api(encoded_id):
 	reader = get_reader()
-	feed_id = decode_id(encoded_id)
-	reader.delete_feed(feed_id)
+	feed_url = decode_id(encoded_id)
+	reader.delete_feed(feed_url)
 	return {}
 	
 ## Entry API
 @app.get("/entries")
-async def get_entries():
+async def get_entries_api():
 	reader = get_reader()
 	entries = reader.get_entries()
 	return encode_data(entries, exclude={"feed"}, exclude_none=True)
