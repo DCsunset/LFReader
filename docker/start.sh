cb820568f4f440191b26671470ed10bcff709cad
DCsunset <DCsunset@users.noreply.github.com>
2022-12-04 10:30:36 -0500

    fix(docker): update env variables

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+export YAFR_DB=/app/data/db.sqlite
+export YAFR_NO_OPENAPI=1
+
+mkdir -p /app/data
+
 cd /app/backend
 uvicorn app:app --host 0.0.0.0 --port 3000 &> /var/log/backend.log &
 cd /app
