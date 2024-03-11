dac6c6a7e479892c51f4ed87de134072f9ea67b2
DCsunset <DCsunset@protonmail.com>
2024-03-10 21:33:24 -0400

    chore: add versionrc

---
+++ .versionrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  types: [
+    {"type": "feat", "section": "Features"},
+    {"type": "fix", "section": "Bug Fixes"},
+    {"type": "chore", "section": "Misc"},
+    {"type": "docs", "section": "Misc"},
+    {"type": "style", "section": "Misc"},
+    {"type": "refactor", "section": "Misc"},
+    {"type": "perf", "section": "Misc"},
+    {"type": "test", "section": "Misc"},
+    {"type": "ci", "section": "Misc"},
+    {"type": "build", "section": "Misc"}
+  ]
+};
