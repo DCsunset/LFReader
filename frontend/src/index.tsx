eb28da1b87818462a497119e5b27bfd86ce32627
DCsunset <DCsunset@protonmail.com>
2024-08-29 03:28:33 +0000

    feat(frontend): add unocss

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -16,6 +16,7 @@
 
 import { render } from 'preact';
 import App from "./App";
+import 'virtual:uno.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import UnoCSS from 'unocss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,5 +17,8 @@ export default defineConfig({
       "Content-Security-Policy": "default-src 'self' 'unsafe-inline' 'unsafe-eval' data:"
     }
   },
-	plugins: [preact()],
+	plugins: [
+    preact(),
+    UnoCSS()
+  ],
 });
