b6e1bcc81f967713c978e5585a36049d5257d160
DCsunset <DCsunset@protonmail.com>
2024-11-30 04:25:02 +0000

    flake: add dev env for tauri android

--- flake.nix
+++ flake.nix
@@ -2,9 +2,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+  outputs = inputs@{ nixpkgs, flake-parts, fenix, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
@@ -12,7 +16,16 @@
       ];
 
       perSystem = { self', system, pkgs, ... }: let
-        # common dependencies
+        # specify versions to prevent downloading too many files
+        buildToolsVersion = "34.0.0";
+        androidPkgs = (pkgs.androidenv.composeAndroidPackages {
+          # should be same with frontend/src-tauri/gen/android/app/build.gradle.kts
+          platformVersions = [ "34" ];
+          buildToolsVersions = [ buildToolsVersion ];
+          abiVersions = [ "arm64-v8a" ];
+          includeNDK = true;
+        });
+
         deps = with pkgs; [
           (python3.withPackages (p: with p; [
             aiohttp
@@ -23,6 +36,41 @@
             pydantic
             python-lsp-server
           ]))
+
+          # For Tauri
+          pkg-config
+          gobject-introspection
+          cargo-tauri
+          nodejs
+
+          # Rust toolchain (targets should match abiVersions in composeAndroidPackages)
+          (with fenix.packages.${system};
+            combine [
+              minimal.rustc
+              minimal.cargo
+              targets.aarch64-linux-android.latest.rust-std
+              # targets.armv7-linux-androideabi.latest.rust-std
+              # targets.i686-linux-android.latest.rust-std
+              # targets.x86_64-linux-android.latest.rust-std
+            ])
+
+          # Tauri Android
+          jdk
+          androidPkgs.androidsdk
+        ];
+        buildDeps = with pkgs; [
+          at-spi2-atk
+          atkmm
+          cairo
+          gdk-pixbuf
+          glib
+          gtk3
+          harfbuzz
+          librsvg
+          libsoup_3
+          pango
+          webkitgtk_4_1
+          openssl
         ];
 
         createFlakeApp = name: text: {
@@ -33,9 +81,29 @@
           };
         };
       in {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            android_sdk.accept_license = true;
+            allowUnfree = true;
+          };
+        };
+
         devShells = {
           default = pkgs.mkShell {
-            packages = deps;
+            # packages = deps;
+            # inputsFrom = buildDeps;
+            nativeBuildInputs = deps;
+            buildInputs = buildDeps;
+
+            ANDROID_HOME = "${androidPkgs.androidsdk}/libexec/android-sdk";
+            NDK_HOME = "${androidPkgs.androidsdk}/libexec/android-sdk/ndk/${androidPkgs.ndk-bundle.version}";
+            # Fix dynamic linking error (Credits to https://gist.github.com/janKeli/1a0d66b7f059387e44ba232b79af7450)
+            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidPkgs.androidsdk}/libexec/android-sdk/build-tools/34.0.0/aapt2";
+
+            shellHook = ''
+              export PATH="$PATH:$ANDROID_HOME/build-tools/${buildToolsVersion}"
+            '';
           };
         };
         apps = {
