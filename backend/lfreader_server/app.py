fa497dbbf018dafb440c46fe1facfa93b9c110aa
DCsunset <DCsunset@protonmail.com>
2024-12-03 17:09:41 +0000

    fix(backend): add cors middleware to support standalone api server

--- backend/lfreader_server/app.py
+++ backend/lfreader_server/app.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from fastapi import FastAPI, Query, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.docs import (
   get_swagger_ui_html,
   get_swagger_ui_oauth2_redirect_html
@@ -55,6 +56,12 @@ if config_file:
 app = FastAPI(root_path="/api", docs_url=None, redoc_url=None)
 state = AppState()
 storage = Storage(config)
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_methods=["*"]
+)
 
 async def taskRunner(task):
   try:
