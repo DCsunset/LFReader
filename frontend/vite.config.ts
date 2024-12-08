667e147e534c9d33401167d5a1c7424d5bfb34aa
DCsunset <DCsunset@protonmail.com>
2024-12-08 23:23:11 +0000

    feat(frontend): support PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import UnoCSS from 'unocss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,6 +20,42 @@ export default defineConfig({
   },
 	plugins: [
     preact(),
-    UnoCSS()
+    UnoCSS(),
+    VitePWA({
+      devOptions: {
+        // enabled: true
+      },
+      includeAssets: ['*.ico', '*.png', '*.svg'],
+      manifest: {
+        name: 'LFReader',
+        short_name: 'LFReader',
+        description: 'Self-hosted **L**ocal-first **F**eed **Reader** written in Python and Preact/React',
+        theme_color: '#222222',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ],
 });
