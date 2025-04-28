acf43e1b0881f9117cbe79353f09e79a4037b28c
DCsunset <DCsunset@protonmail.com>
2025-04-28 22:28:43 +0000

    refactor(frontend): migrate responsive layout for different screen sizes

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -16,8 +16,11 @@
 
 import MenuIcon from "lucide-solid/icons/menu"
 import ListIcon from "lucide-solid/icons/list"
-import { createSignal } from "solid-js"
+import { createMemo, createSignal } from "solid-js"
 import FeedList from "./FeedList"
+import { Show, JSX } from "solid-js"
+import defaultTheme from "tailwindcss/defaultTheme"
+import { createBreakpoints } from "@solid-primitives/media"
 
 const appBarHeight = "44px"
 const feedListWidth = "250px";
@@ -26,42 +29,102 @@ const entryListWidth = "350px";
 const [feedList, setFeedList] = createSignal(false)
 const [entryList, setEntryList] = createSignal(false)
 
-function AppBar() {
+function Drawer(props: {
+  open: boolean,
+  onChange: (_: boolean) => void,
+  children: any,
+  modal: boolean,
+  style?: JSX.CSSProperties,
+}) {
   return (
-    <div class=" px-3 flex items-center bg-primary" style={{ height: appBarHeight }}>
-      <label for="feed-list" class="pa-1 mr-2 d-btn d-btn-ghost d-btn-circle d-btn-sm hover:bg-white/15 border-none">
-        <MenuIcon />
-      </label>
-
-      <h1 class="grow text-xl font-semibold">All</h1>
-
-      <label for="entry-list" class="pa-1 mr-2 d-btn d-btn-ghost d-btn-circle d-btn-sm hover:bg-white/15 border-none">
-        <ListIcon />
-      </label>
+    <div class="d-drawer">
+      <input
+        type="checkbox"
+        class="d-drawer-toggle"
+        checked={props.open}
+        onChange={e => props.onChange(e.currentTarget.checked)}
+      />
+      <div
+        class="d-drawer-side"
+        style={{
+          width: props.modal ? undefined : "auto",
+          ...props.style
+        }}
+      >
+        <Show when={props.modal}>
+          <div class="d-drawer-overlay" onClick={() => props.onChange(false)} />
+        </Show>
+        {props.children}
+      </div>
     </div>
   )
 }
 
 export default function Layout() {
+  const breakpoints = createBreakpoints(defaultTheme.screens)
+  const actualFeedListWidth = createMemo(() => breakpoints.sm && feedList() ? feedListWidth : "0")
+  const actualEntryListWidth = createMemo(() => breakpoints.sm && entryList() ? entryListWidth : "0")
+
   return (
     // <div class="dark">
-    <div class="h-screen">
-      <AppBar />
+    <div class="h-dvh">
+      <Drawer open={feedList()} onChange={setFeedList} modal={!breakpoints.sm}>
+        <FeedList
+          class="text-base-content h-full menu bg-base-200"
+          style={{ width: feedListWidth }}
+        />
+      </Drawer>
 
-      <div class="d-drawer">
-        <input id="feed-list" type="checkbox" class="d-drawer-toggle" />
-        <div class="d-drawer-side">
-          <label for="feed-list" aria-label="close sidebar" class="d-drawer-overlay" />
-          <FeedList
-            class="text-base-content h-full menu bg-base-200"
-            style={{ width: feedListWidth }}
-          />
+      <Drawer
+        open={entryList()}
+        onChange={setEntryList}
+        modal={false}
+        style={{
+          left: actualFeedListWidth(),
+          top: appBarHeight,
+        }}
+      >
+        <div
+          class="text-base-content h-full menu bg-base-200"
+          style={{ width: breakpoints.sm ? entryListWidth : "100dvw" }}
+        >
+          Test
         </div>
-      </div>
+      </Drawer>
 
-      <div style={{ height: `calc(100% - ${appBarHeight})` }}>
-        <div>Hello</div>
-      </div>
+      {/* Apply same transition as the drawer */}
+      <main
+        class="h-full transition-[margin] duration-300 ease-[ease-out]"
+        style={{ "margin-left": actualFeedListWidth() }}
+      >
+        <div class="d-navbar bg-primary min-h-0 px-3" style={{ height: appBarHeight }}>
+          <button
+            class="pa-1 mr-2 d-btn d-btn-ghost d-btn-circle d-btn-sm hover:bg-white/15 border-none"
+            onClick={() => setFeedList(!feedList())}
+          >
+            <MenuIcon />
+          </button>
+
+          <h1 class="grow text-xl font-semibold">All</h1>
+
+          <button
+            class="pa-1 d-btn d-btn-ghost d-btn-circle d-btn-sm hover:bg-white/15 border-none"
+            onClick={() => setEntryList(!entryList())}
+          >
+            <ListIcon />
+          </button>
+        </div>
+
+        <div
+          class="transition-[margin-left] duration-300 ease-[ease-out]"
+          style={{
+            height: `calc(100% - ${appBarHeight})`,
+            "margin-left": actualEntryListWidth(),
+          }}
+        >
+          <div>Hello</div>
+        </div>
+      </main>
     </div>
   )
 }
